5f28e6217193f579f8560467e38f28d96a240f2a
tulga <tulllga@gmail.com>
2023-12-06 14:56:09 +0800

    huvilbar 4 morini zurag tomrulsan

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function begin() {
   document.getElementById("btnEnd").disabled = false;
   document.getElementById("btnClick1").disabled = false;
 
-  // document.getElementById("horse-1").style.display = "none";
-  // document.getElementById("horse-2").style.display = "none";
+  document.getElementById("horse-1").style.display = "none";
+  document.getElementById("horse-2").style.display = "none";
 
   let x = document.getElementById("mytable").getElementsByTagName("td");
-  x[20].innerHTML = '<img height="50px" src="image/horse-1.jpg">';
-  x[40].innerHTML = '<img height="50px" src="image/horse-2.jpg">';
+  x[20].innerHTML = '<img height="80px" src="image/horse-1.jpg">';
+  x[40].innerHTML = '<img height="80px" src="image/horse-2.jpg">';
   (i = 20), (j = 40);
 
   let y = document.getElementById("tableShagai").getElementsByTagName("td");
@@ -53,18 +53,18 @@ function btnClick1() {
   let x;
   if (i < 39) {
     x = document.getElementById("mytable").getElementsByTagName("td");
-    x[i].innerHTML = '<img height="50px" src="image/horse-shadow.jpg">';
+    x[i].innerHTML = '<img height="80px" src="image/horse-shadow.jpg">';
 
     i = i + shagaiOrhih();
 
     if (i >= 39) {
       document.getElementById("p1").innerHTML = "ялагч боллоо";
-      x[39].innerHTML = '<img height="50px" src="image/horse-1.jpg">';
+      x[39].innerHTML = '<img height="80px" src="image/horse-1.jpg">';
       document.getElementById("btnClick1").remove();
       document.getElementById("btnClick2").disabled = true;
       document.getElementById("player-1").style.color = "red";
     } else {
-      x[i].innerHTML = '<img height="50px" src="image/horse-1.jpg">';
+      x[i].innerHTML = '<img height="80px" src="image/horse-1.jpg">';
       document.getElementById("btnClick1").disabled = true;
       document.getElementById("btnClick2").disabled = false;
     }
@@ -77,19 +77,19 @@ function btnClick2() {
   let x;
   if (j < 59) {
     x = document.getElementById("mytable").getElementsByTagName("td");
-    x[j].innerHTML = '<img height="50px" src="image/horse-shadow.jpg">';
+    x[j].innerHTML = '<img height="80px" src="image/horse-shadow.jpg">';
 
     j = j + shagaiOrhih();
 
     if (j >= 59) {
       document.getElementById("p2").innerHTML = "ялагч боллоо";
-      x[59].innerHTML = '<img height="50px" src="image/horse-2.jpg">';
+      x[59].innerHTML = '<img height="80px" src="image/horse-2.jpg">';
       document.getElementById("btnClick1").disabled = true;
       document.getElementById("btnClick2").remove();
       document.getElementById("btnClick1").disabled = true;
       document.getElementById("player-2").style.color = "red";
     } else {
-      x[j].innerHTML = '<img height="50px" src="image/horse-2.jpg">';
+      x[j].innerHTML = '<img height="80px" src="image/horse-2.jpg">';
 
       document.getElementById("btnClick2").disabled = true;
       document.getElementById("btnClick1").disabled = false;
